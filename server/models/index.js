30f0a18cb4823599f9772054b2a656774b1dc9e8
Master <nghiemxthai@gmail.com>
2024-03-10 11:09:17 -0700

    fixed photo for products

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-const Coupon = require('./Coupon');
-const Category = require('./Category');
-const Order = require('./Order');
-const Product = require('./Product');
-const Review = require('./Review');
-const User = require('./User');
-
-module.exports = { Coupon, Category, Order, Product, Review, User };
